a73ad9e467745dce6a1b59f0c6754842bf871a28
LeeJoEun-01 <joeun1005@naver.com>
2024-02-03 23:55:32 +0900

    Implementation 문제 풀기

---
+++ Implementation/Implementation_1158.py
@@ -0,0 +1,21 @@
+# Greedy
+# 1158. 요세푸스 문제
+
+from collections import deque
+
+n, k = map(int, input().split())
+
+# 숫자 배열 만들기
+arr = deque()
+for i in range(1, n+1):
+    arr.append(i)
+
+# k번째 사람 제거하기
+result = []
+
+while arr:
+    for _ in range(k-1):
+        arr.append(arr.popleft())
+    result.append(arr.popleft())
+
+print(str(result).replace('[', '<').replace(']', '>'))
