1d6e1332dc7dac5694644992924537c9f5770162
LeeJoEun-01 <joeun1005@naver.com>
2024-02-08 19:30:06 +0900

    [Implementation] 쉬운 문제 해결

--- Implementation/Implementation_11720.py
+++ Implementation/Implementation_11720.py
@@ -1,13 +1,19 @@
 # Implementation
 # 11720. 숫자의 합
 
-n = int(input())
-num_list = list(map(int, input()))
+# n = int(input())
+# num_list = list(map(int, input()))
 
-print(sum(num_list))
+# print(sum(num_list))
 
 # Implementation
 # 11654. 아스키코드
 
-input = input()
-print(ord(input))
+# input = input()
+# print(ord(input))
+
+# Implementation
+# 1152. 단어의 개수
+
+word_arr = input().split()
+print(len(word_arr))
