fd58afa1f12c60826d3605c56859e3ddf5f3ef1e
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2024-02-17 16:48:59 +0900

    [Silver IV] Title: 바닥 장식, Time: 124 ms, Memory: 110592 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,27 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+floor = [list(map(str, sys.stdin.readline())) for _ in range(n)]
+count = 0
+
+
+# - 검사
+for i in floor:
+    target = ""
+    for j in range(m):
+        if target != i[j]:
+            if i[j] != "|":
+                count += 1
+            target = i[j]
+
+# | 검사
+for i in range(m):
+    target = ""
+    for j in range(n):
+        if target != floor[j][i]:
+            if floor[j][i] != "-":
+                count += 1
+            target = floor[j][i]
+
+print(count)
