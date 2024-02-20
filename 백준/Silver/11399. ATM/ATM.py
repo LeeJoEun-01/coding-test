2df9ac11a729479d0c0c735dcd720b82ed80cbd5
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2024-02-20 22:59:55 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+num_list = list(map(int, sys.stdin.readline().split()))
+
+num_list.sort()
+
+sum = 0
+for i in range(n+1):
+    for j in range(i):
+        sum += num_list[j]
+
+print(sum)
