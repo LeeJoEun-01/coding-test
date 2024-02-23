9c0247cd2abb46c7ac2cfdc7f9038c8e71128e78
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2024-02-23 15:25:27 +0900

    [Silver I] Title: 접두사, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+
+words = [sys.stdin.readline().strip() for _ in range(n)]
+words.sort(key=len)
+
+# print(words)
+
+for i in range(n):
+    length = len(words[i])
+    # print("========")
+    for j in range(i+1, n):
+        word = words[j]
+        target = word[0:length]
+        # print(word)
+        # print(target)
+        # print()
+        if words[i] == target:
+            words[i] = 0
+
+print(n-words.count(0))
