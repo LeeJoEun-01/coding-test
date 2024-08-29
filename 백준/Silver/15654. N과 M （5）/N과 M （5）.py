7707a166225d69ce47ac86709789d9fbcffadfc6
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2024-08-29 16:49:29 +0900

    [Silver III] Title: N과 M (5), Time: 156 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,13 +1,22 @@
 import sys
-from itertools import *
 
 N, M = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
+
 arr.sort()
+visited = [False]*(N+1)
+result = []
 
-result = list(permutations(arr, M))
+def dfs():
+  if len(result) == M:
+    print(' '.join(map(str, result)))
 
-for row in result:
-  for ele in row:
-    print(ele, end=" ")
-  print()
+  for i in range(1, N+1):
+    if not visited[i]:
+      visited[i] = True
+      result.append(arr[i-1])
+      dfs()
+      result.pop()
+      visited[i] = False
+
+dfs()
