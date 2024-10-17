a6c91798d4c0c747bf4613e79d560b4ae47e6d9f
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2024-10-17 20:15:18 +0900

    [Gold IV] Title: 최단경로, Time: 604 ms, Memory: 70344 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,33 @@
+import sys
+import math
+import heapq
+
+V, E = map(int, sys.stdin.readline().split())
+K = int(sys.stdin.readline().strip())
+graph = [[] for _ in range(V+1)]
+
+for _ in range(E):
+  u, v, w = map(int, sys.stdin.readline().split())
+  graph[u].append((w,v))
+
+weight = [math.inf]*(V)
+weight[K-1] = 0
+queue = []
+heapq.heappush(queue, (0,K))
+
+while queue:
+  w, v = heapq.heappop(queue)
+
+  if weight[v-1] < w:
+    continue
+
+  for newW, newV in graph[v]:
+    if weight[newV-1] > w+newW:  
+      weight[newV-1] = newW + w
+      heapq.heappush(queue, (newW + w, newV))
+
+for i in weight:
+  if i == math.inf:
+    print("INF")
+  else:
+    print(i)
