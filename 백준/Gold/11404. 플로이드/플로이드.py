48a2590091566a81eafbb208ffc24a9e380706c9
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2024-10-31 17:18:43 +0900

    [Gold IV] Title: 플로이드, Time: 712 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+N = int(sys.stdin.readline().rstrip())
+B = int(sys.stdin.readline().rstrip())
+graph = [[math.inf for _ in range(N)] for _ in range(N)]
+
+for i in range(B):
+  v1, v2, w = map(int, sys.stdin.readline().split())
+  if graph[v1-1][v2-1] > w:
+    graph[v1-1][v2-1] = w
+
+for k in range(N):
+  for i in range(N):
+    for j in range(N):
+      graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+
+      if i == j:
+        graph[i][j] = 0
+
+for i in range(N):
+  for j in range(N):
+    if i == j or graph[i][j] == math.inf:
+      print(0, end=" ")
+    else:
+      print(graph[i][j], end=" ")
+  print()
