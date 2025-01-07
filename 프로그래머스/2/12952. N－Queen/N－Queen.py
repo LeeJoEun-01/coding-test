e64f4708b0d3770a25534a27c6bc2eef88951263
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2025-01-07 17:20:14 +0900

    [level 2] Title: N-Queen, Time: 9431.40 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,22 +1,27 @@
 def solution(n):
+    global count
+    count = 0
     cols = [0]*n
-    count = dfs(0, 0, cols, n)
-    return count
     
-def promising(row, i, cols):
-    for b_row in range(row):
-        # 가로, 세로, 대각선 검사
-        if cols[b_row] == i or abs(cols[b_row]-i)==abs(b_row-row):
-            return False
-    return True
-
-def dfs(row, cnt, cols, n):
-    answer = 0
-    if row == n:
-        return 1
-    else:
-        for i in range(n):
-            if promising(row, i, cols):
+    def promising(row):
+        for b_row in range(row):
+            # 가로, 세로, 대각선 검사
+            if cols[b_row] == cols[row] or abs(cols[b_row]-cols[row])==abs(b_row-row):
+                return False
+        return True
+    
+    def dfs(row):
+        global count
+        
+        if row == n:
+            row = 0
+            count += 1
+            return
+        else:
+            for i in range(n):
                 cols[row] = i
-                answer += dfs(row+1, cnt, cols, n)
-    return answer
+                if promising(row):
+                    dfs(row+1)
+        
+    dfs(0)
+    return count
