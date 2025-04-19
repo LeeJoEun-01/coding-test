290b0a1c7f031f702df5fcb6564c3531612c456a
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2025-04-20 01:43:06 +0900

    [level 3] Title: n + 1 카드게임, Time: 4.04 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -1,29 +1,57 @@
-def delete_card(a,b,t):
-    for i in a:
-        if t-i in b:
-            a.remove(i)
-            b.remove(t-i)
-            return True
-    return False
-            
 def solution(coin, cards):
-    answer=1
-    n=len(cards)
-    mycard=[cards[i] for i in range(n//3)]
-    index=n//3
-    leftovers=[]
-    while coin>=0 and index<n :
-        leftovers.append(cards[index])
-        leftovers.append(cards[index+1])
-        if delete_card(mycard,mycard,n+1):
-            pass
-        elif coin>=1 and delete_card(mycard,leftovers,n+1):
-            coin-=1
-        elif coin>=2 and delete_card(leftovers,leftovers,n+1):
-            coin-=2
+    answer = 1
+    n = len(cards)
+    N = n + 1
+    hand = set(cards[:n // 3])
+    stack = cards[n // 3:]
+    temp = set()
+
+    for i in range(len(stack) // 2):
+        # 2개 카드 꺼내기
+        for x in stack[2 * i:2 * i + 2]:
+            if N - x in hand and coin >= 1:
+                coin -= 1
+                hand.add(x)
+            else:
+                temp.add(x)
+
+        # 1단계: hand 내에서 짝 제거
+        check1 = False
+        for x in hand:
+            if N - x in hand:
+                hand.remove(x)
+                hand.remove(N - x)
+                answer += 1
+                check1 = True
+                break
+
+        if check1:
+            continue
+
+        # 2단계: hand, temp 조합
+        check2 = False
+        for x in hand:
+            if N - x in temp and coin >= 1:
+                coin -= 1
+                hand.remove(x)
+                temp.remove(N - x)
+                answer += 1
+                check2 = True
+                break
+
+        if check2:
+            continue
+
+        # 3단계: temp 내에서 2개 코인 써서 구매
+        for x in temp:
+            if N - x in temp and coin >= 2:
+                coin -= 2
+                temp.remove(x)
+                temp.remove(N - x)
+                answer += 1
+                break
         else:
-            break
-        
-        answer+=1
-        index+=2
+            # 아무것도 못 했으면 종료
+            return answer
+
     return answer
