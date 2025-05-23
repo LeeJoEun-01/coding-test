106d5cb95c45f77444deb76b93277cfd6c6eaecd
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2025-05-23 15:12:17 +0900

    [Silver V] Title: 색종이, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+arr = [list(map(int, input().split())) for _ in range(N)]
+maps = [[0]*100 for i in range(100)]
+
+for i in range(N):
+    x, y = arr[i]
+
+    for xx in range(x, x+10):
+        for yy in range(y, y+10):
+            maps[xx][yy] = 1
+
+ans = 0
+for i in range(100):
+    count = maps[i].count(1)
+    ans += count
+
+print(ans)
