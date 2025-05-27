c5a1e8a88c0cd19efca02689d9bbb260fbc331a0
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2025-05-27 09:43:07 +0900

    [Gold II] Title: 저울, Time: 1164 ms, Memory: 239388 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+arr = list(map(int, input().split()))
+
+arr.sort()
+maxN = arr[0]
+nums = [arr[0]]
+isDone = False
+
+if arr[0] == 1:
+    for i in range(1, N):
+        tmp = []
+        for n in nums:
+            nn = n+arr[i]
+            if nn > maxN:
+                if nn == maxN + 1:
+                    tmp.append(nn)
+                    maxN = nn
+                elif arr[i] == maxN + 1:
+                    tmp.append(arr[i])
+                    maxN = arr[i]
+                    if nn == maxN + 1:
+                        tmp.append(nn)
+                        maxN = nn
+                    else:
+                        isDone = True
+                        break
+                else:
+                    isDone = True
+                    break
+        tmp.sort()
+        nums += tmp
+        if isDone:
+            break
+else:
+    maxN = 0
+
+print(maxN+1)
