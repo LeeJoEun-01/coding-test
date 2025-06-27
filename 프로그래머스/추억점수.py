d74624ef4715a153e7f31c0c8dc8ff1beb5ca8d1
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2025-06-27 14:58:32 +0900

    [프로그래머스] 추억점수 문제 풀기

---
+++ 프로그래머스/추억점수.py
@@ -0,0 +1,15 @@
+def solution(name, yearning, photo):
+    answer = []
+    dict = {}
+    
+    for i in range(len(name)):
+        dict[name[i]] = i
+        
+    for line in photo:
+        ans = 0
+        for na in line:
+            if na in dict:
+                index = dict[na]
+                ans += yearning[index]
+        answer.append(ans)
+    return answer
