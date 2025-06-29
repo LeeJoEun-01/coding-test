51ee8bcc3df0d77246803bac31012e242453e043
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2025-06-29 20:11:50 +0900

    [Silver IV] Title: 문자열, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+X, Y = input().split()
+
+answer = []
+for i in range(len(Y) - len(X) + 1):
+    count = 0
+    for j in range(len(X)):
+        if X[j] != Y[i + j]:
+            count += 1
+    answer.append(count)
+
+print(min(answer))
