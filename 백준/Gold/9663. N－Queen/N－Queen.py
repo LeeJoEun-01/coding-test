36416e8642c0eb0d7fdf58773d67d28929823baf
JoEun Lee <78733700+LeeJoEun-01@users.noreply.github.com>
2026-03-28 21:53:37 +0900

    [Gold IV] Title: N-Queen, Time: 28208 ms, Memory: 200540 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,27 +1,30 @@
 import sys
-N = int(sys.stdin.readline().rstrip())
+input = sys.stdin.readline
 
-board = [0]*N # 일차원 배열, 어차피 한 줄에 하나만 존재할 수 있다.
+N = int(input().rstrip())
 cnt = 0
+visited = [-1]*N
 
-def check(x):
-  for i in range(x):
-    if board[x] == board[i] or x-i == abs(board[x]-board[i]):
-      return False
-  return True
 
-def backTracking(x):
-  global cnt
+def check(now_row):
+    for row in range(now_row):
+        if visited[row] == visited[now_row] or (now_row-row) == abs(visited[now_row]-visited[row]):
+            return False
+    return True
 
-  if x == N:
-    cnt += 1
-    return
-  
-  else:
-    for i in range(N):
-      board[x] = i
-      if check(x):
-        backTracking(x+1)
 
-backTracking(0)
+def dfs(row):
+    global cnt
+
+    if row == N:
+        cnt += 1
+
+    else:
+        for col in range(N):
+            visited[row] = col
+            if check(row):
+                dfs(row+1)
+
+
+dfs(0)
 print(cnt)
